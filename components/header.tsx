5f92e73b5d19970f59f4276a6285544f4bb8bc3c
kohrua <kohura12345678@gmail.com>
2023-01-10 17:31:09 +0900

    Implemented display of Note.

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="border-b flex items-center h-14 px-4 shadow-md font-bold">
+      Note
+    </header>
+  );
+};
+
+export default Header;
