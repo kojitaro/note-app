259930986440ad8d72867c8f72bd6903a57ea4d3
kohrua <kohura12345678@gmail.com>
2023-01-14 17:12:58 +0900

    add analytics tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,16 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head />
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_TAG_ID}`}></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_TAG_ID}');
+          `
+        }}/>
       <body>
         <Header />
         <Main />
